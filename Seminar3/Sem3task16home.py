123b987de8bfcdc55abe71a6c4142c131fa76d16
marina.leonova <leona_21@mail.ru>
2023-02-17 19:42:09 +0300

    Семинар 3, решены задачи 17, 19, 21, 23 в группе; 16, 18, 20 - самостоятельно.

--- Seminar3/Sem3task16home.py
+++ Seminar3/Sem3task16home.py
@@ -4,18 +4,32 @@
 # строках записаны N целых чисел Ai
 # . Последняя строка содержит число X
 
+
+# n = int(input('введите натуральное число N – количество элементов в массиве'))
+# x = int(input('число X, которое встречается в массиве A'))
+# from random import randint
+# a = [randint(1, 10) for i in range(n)]
+# print(a)
+# count = 0
+# for i in a:
+#     if i == x:
+#         i += 1
+#         count += 1
+#         print(f'число x={x} встречается в заданном массиве {count} раз')
+#     else:
+#         print('заданное число в массиве отсутствует')
+#         break
+
 n = int(input('введите натуральное число N – количество элементов в массиве'))
-x = int(input('число X, которое встречается в массиве A'))
 from random import randint
 a = [randint(1, 10) for i in range(n)]
 print(a)
+x = int(input('число X, которое встречается в массиве A'))
 count = 0
 for i in a:
     if i == x:
-        i += 1
         count += 1
-        print(f'число x={x} встречается в заданном массиве {count} раз')
-    else:
+    if count == 0:
         print('заданное число в массиве отсутствует')
-        break
-
+else:
+    print(f'число x={x} встречается в заданном массиве {count} раз')
