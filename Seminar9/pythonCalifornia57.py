2e60e595128218ed7093f2972005e7472785f374
marina.leonova <leona_21@mail.ru>
2023-03-02 11:29:51 +0300

    Семинар 9, уточнение решение задачи на занятии и дома

---
+++ Seminar9/pythonCalifornia57.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv('california_housing_train.csv')
+print(df.head())
+
+import matplotlib.pyplot as plt
+import seaborn as sns
--- Seminar9/task40&42.py
+++ Seminar9/task40&42.py
@@ -1,15 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+df = pd.read_csv('sample_data/california_housing_train.csv')
+#
+# """задача 40"""
+#
+# df[df['population'] <= 500]['median_house_value'].mean()
+#
+# df['population'] <= 500
+# print(df['population'] <= 500)
+#
 
 
-"""задача 40"""
-
-df[df['population'] <= 500]['median_house_value'].mean()
-
-df['population'] <= 500
-
-"""Задача 42"""
-
-df['population'].quantile(.25)
-
-df['population'] < df['population'].quantile(.25)
-
-print(df[df['population'] < df['population'].quantile(.25)]['households'].max())
+# """Задача 42"""
+#
+# df['population'].quantile(.25)
+#
+# df['population'] < df['population'].quantile(.25)
+#
+# print(df[df['population'] < df['population'].quantile(.25)]['households'].max())
